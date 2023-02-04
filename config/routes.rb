4a0af5672c3920f10165c852a3007754b741e7fe
renren121314 <renren199612@gmail.com>
2023-02-04 05:41:42 +0000

    2/4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+  end
+
+  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
